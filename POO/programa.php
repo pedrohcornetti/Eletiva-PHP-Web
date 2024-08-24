2eefdf9993da6aaa4757ef2480cfead712b3ee51
pedrohcornetti <pedrocornetti@outlook.com>
2024-08-24 08:39:44 -0300

    criação classe Pessoa

--- POO/programa.php
+++ POO/programa.php
@@ -1,8 +1,12 @@
 <?php
 
-    //include "Usuario.php";
-    //include "Dados/Usuario.php";
+    include "Usuario.php";
+    include "Dados/Usuario.php";
 
     use POO\Usuario as UsuarioRaiz;
-    use POO\dados\Usuario;
+    use POO\dados\Usuario as UsuarioDados;
+
     $obj = new UsuarioRaiz();
+    var_dump($obj);
+    $obj2 = new UsuarioDados();
+    var_dump($obj2);
